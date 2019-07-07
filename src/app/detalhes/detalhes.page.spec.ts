2bbed1b384572ec7753c2ff9d917607f22450efa
Alefe Souza <contact@alefesouza.com>
2019-07-07 01:01:55 -0300

    Código introdução Ionic

--- src/app/detalhes/detalhes.page.spec.ts
+++ src/app/detalhes/detalhes.page.spec.ts
@@ -1,22 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomePage } from './home.page';
+import { DetalhesPage } from './detalhes.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('DetalhesPage', () => {
+  let component: DetalhesPage;
+  let fixture: ComponentFixture<DetalhesPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ DetalhesPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-      .compileComponents();
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(DetalhesPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  date = new Date();
 
-  constructor() {}
+  constructor(public alertController: AlertController, private router: Router) {}
+
+  open() {
+    this.router.navigateByUrl('/detalhes');
+  }
+
+  async exibirAlerta() {
+    const alerta = await this.alertController.create({
+      header: 'Olá',
+      subHeader: 'Como você está?',
+      buttons: ['Bem', 'Mal'],
+    });
+
+    await alerta.present();
+  }
+
 
 }
